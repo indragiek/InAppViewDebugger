b40d702c2f3a58c45066ec7787651644c82a58cb
Indragie Karunaratne <i@indragie.com>
2019-03-31 18:33:35 -0700

    Initial Commit

---
+++ InAppViewDebugger/InAppViewDebugger.h
@@ -0,0 +1,19 @@
+//
+//  InAppViewDebugger.h
+//  InAppViewDebugger
+//
+//  Created by Indragie Karunaratne on 3/31/19.
+//  Copyright © 2019 Indragie Karunaratne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InAppViewDebugger.
+FOUNDATION_EXPORT double InAppViewDebuggerVersionNumber;
+
+//! Project version string for InAppViewDebugger.
+FOUNDATION_EXPORT const unsigned char InAppViewDebuggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppViewDebugger/PublicHeader.h>
+
+
