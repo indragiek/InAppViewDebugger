b504d07fc651227b4ba4f557eb274f93739b014c
Indragie Karunaratne <i@indragie.com>
2019-04-20 15:19:56 -0700

    Put back window property, required for storyboards to work

--- Example-Swift/AppDelegate.swift
+++ Example-Swift/AppDelegate.swift
@@ -11,6 +11,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
