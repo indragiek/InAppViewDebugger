57505de8883c04ece61b87e0277e8b33e128796d
Indragie Karunaratne <i@indragie.com>
2019-04-20 18:04:54 -0700

    Remove umbrella header comment

--- InAppViewDebugger/InAppViewDebugger.h
+++ InAppViewDebugger/InAppViewDebugger.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double InAppViewDebuggerVersionNumber;
 
 //! Project version string for InAppViewDebugger.
 FOUNDATION_EXPORT const unsigned char InAppViewDebuggerVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <InAppViewDebugger/PublicHeader.h>
-
-
